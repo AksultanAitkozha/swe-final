13affa5a61bb89edb40fd4b48ffe7ea3772c882c
Aksultan Aitkozhin <aksultan.aitkozhin@nu.edu.kz>
2023-11-28 15:13:25 +0600

    Update rightbar.jsx

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -12,12 +12,11 @@ const Rightbar = () => {
         <div className={styles.text}>
           <span className={styles.notification}>🔥 Available Now</span>
           <h3 className={styles.title}>
-            How to use the new version of the admin dashboard?
+            How to use the VMS for the admin dashboard?
           </h3>
           <span className={styles.subtitle}>Takes 4 minutes to learn</span>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit eius libero perspiciatis recusandae possimus.
+            Let our drivers work hardly for better earns!
           </p>
           <button className={styles.button}>
             <MdPlayCircleFilled />
@@ -29,13 +28,11 @@ const Rightbar = () => {
         <div className={styles.text}>
           <span className={styles.notification}>🚀 Coming Soon</span>
           <h3 className={styles.title}>
-            New server actions are available, partial pre-rendering is coming
-            up!
+            Dear Driver, new locations will be available! Get ready for new 
+            rides!
           </h3>
           <span className={styles.subtitle}>Boost your productivity</span>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit eius libero perspiciatis recusandae possimus.
           </p>
           <button className={styles.button}>
             <MdReadMore />
